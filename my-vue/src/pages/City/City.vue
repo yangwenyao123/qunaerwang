6131479357c955a4334dd63b1a7f53e0d39b18bd
ywy <1986269990@qq.com>
2019-07-05 21:04:29 +0800

    city-flex

--- my-vue/src/pages/City/City.vue
+++ my-vue/src/pages/City/City.vue
@@ -1,21 +1,48 @@
 <template>
   <div class="content">
-    <cityH></cityH>
-    <cityList></cityList>
+    <cityH ></cityH>
+    <cityList :cities="cities" :hotCities="hotCities" :letter="letter"></cityList>
+    <flex :cities="cities" @hanclick="hanclick" ></flex>
   </div>
 </template>
 <script>
   import cityH from './components/cityHeader'
   import cityList from './components/cityList'
+  import flex from './components/flex.vue'
   export default {
     data () {
       return {
+        hotCities:[],
+        cities:null,
+         letter:''
 
       }
     },
+    methods:{
+      hanclick(e){
+        this.letter = e
+      } 
+    },
     components:{
       cityH,
-      cityList
+      cityList,
+      flex
+    },
+    mounted(){
+      this.$axios.get('/api/city.json').then(res=>{
+        if(res.status==200&&res.statusText=="OK"){
+          var data = res.data.data
+          console.log(data);
+          
+          this.hotCities = data.hotCities 
+          this.cities = data.cities
+         
+          
+          
+        }
+       
+        
+      })
     }
   }
 </script>
--- my-vue/src/pages/City/components/cityList.vue
+++ my-vue/src/pages/City/components/cityList.vue
@@ -1,122 +1,133 @@
 <template>
-  <div class="content">
-    <div class="top">
-      <div class="title">
-        您的位置
-      </div>  
-      <div class="bottom">
-        <button>广州</button>
+<div>
+  <div class="content" ref="cityList">
+    <div>
+      <div class="top">
+        <div class="title">您的位置</div>
+        <div class="bottom">
+          <button>广州</button>
+        </div>
       </div>
-    </div> 
 
-    <div class="list">
-      <div class="title">热门城市</div>
-      <div class="time">
-        <button v-for="(item,index) in city" 
-        :key="index" >{{item.name}}</button> 
-        
+      <div class="list">
+        <div class="title">热门城市</div>
+        <div class="time">
+          <button v-for="(item,index) in hotCities" :key="index">{{item.name}}</button>
+        </div>
+      </div>
+      <div class="cityList">
+        <div class="tiem" v-for="(item,key) in cities" :key="key" :ref="key">
+          <div class="title">{{key}}</div>
+          <ul>
+            <li v-for="(item,index) in item" :key="index">{{item.name}}</li>
+         
+          </ul>
+        </div>
+      
       </div>
     </div>
-
-  </div>  
-
+  </div>
+ 
+</div>
 </template>
 <script>
+
+import Bscroll from "better-scroll";
 export default {
-  data(){
-    return {
-      city:[
-        {name:"广州"},
-        {name:"广州"},
-        {name:"广州"},
-        {name:"广州"},
-        {name:"广州"},
-        {name:"广州"},
-      ],
-      active:false
-    }
+  props:{
+    hotCities:Array,
+    cities:Object,
+    letter:String
   },
-  methods:{
-     
-    
-     margin(){
-      //  
-      console.log(this.city.length );
-      
-      for(var i=1;i<this.city.length-1;i++){
-        if(i%3==0){
-          this.active = true
-          console.log(this.active);
-          
-        }else{
-          this.aa = true
-          this.active = false
+
+    data() {
+      return {
+        city: [
+          { name: "广州" },
+          { name: "广州" },
+          { name: "广州" },
+          { name: "广州" },
+          { name: "广州" },
+          { name: "广州" }
+        ]
+      };
+    },
+    mounted() {
+      this.scroll = new Bscroll(this.$refs.cityList);
+    },
+    watch: {
+      letter () {
+        if(this.letter){
+            var element = this.$refs[this.letter][0]
+            this.scroll.scrollToElement(element)
+        
         }
-      }
       
+      }
     }
-  },
-  mounted(){
-   this.margin()
-  },
-  computd:{
     
-  }
-}
+  };
 </script>
 <style scoped>
-.time{
-  padding: .4rem;
-}
+  .content {
+    position: absolute;
+    top: 2rem !important;
+    bottom: 0;
+    right: 0;
+    overflow: hidden;
+  }
 
+  .cityList li {
+    height: 0.8rem;
+    border-bottom: 1px solid #ccc;
+    line-height: 0.8rem;
+    padding-left: 0.4rem;
+  }
+  .time {
+    padding: 0.4rem;
+  }
 
-.time button{
+  .time button {
     width: 2.3rem;
-    height: .7rem;
-    line-height: .7rem;
+    height: 0.7rem;
+    line-height: 0.7rem;
     text-align: center;
     border: 1px solid #ccc;
     color: #000;
     background-color: #fff;
-    border-radius: 2px ;
-    margin:0 .2rem .3rem 0;
-}
-
-.time button:nth-child(3){
-  margin-right:0
-}
-.time button:nth-child(6){
-  margin-right:0
-}
-  .top{
-    height: 2rem;
- 
+    border-radius: 2px;
+    margin: 0 0.2rem 0.3rem 0;
   }
- .title{
-     height: .6rem;
-     line-height: .6rem;
-     padding-left: .4rem;
-     background-color: #eee;
-  
-   }
-  .top .bottom{
+
+  .time button:nth-child(3) {
+    margin-right: 0;
+  }
+  .time button:nth-child(6) {
+    margin-right: 0;
+  }
+  .top {
+    height: 2rem;
+  }
+  .title {
+    height: 0.6rem;
+    line-height: 0.6rem;
+    padding-left: 0.4rem;
+    background-color: #eee;
+  }
+  .top .bottom {
     height: 1.2rem;
     display: flex;
     align-items: center;
-    padding-left: .4rem;
-   
+    padding-left: 0.4rem;
   }
-  .top .bottom  button{
+  .top .bottom button {
     width: 2.5rem;
-    height: .7rem;
-    line-height: .7rem;
+    height: 0.7rem;
+    line-height: 0.7rem;
     text-align: center;
     border: 1px solid skyblue;
     color: skyblue;
     background-color: #fff;
-    border-radius: 2px ;
-  
-    
+    border-radius: 2px;
   }
-
 </style>
